9fbcd72568878efa2fe39734dcd8e98b5c25d770
Bhumika <bhumika.srashtasoft@gmail.com>
2024-04-12 15:02:54 +0530

    first commit

---
+++ myapp/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change directory to your Django project directory
+cd /home/bhumika/Project_TourrUp-master
+
+# Pull latest changes from the Git repository
+git pull origin main
+
+# Run any necessary commands to update dependencies, migrate database, etc.
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Restart the server or any necessary services
+systemctl restart gunicorn.service
